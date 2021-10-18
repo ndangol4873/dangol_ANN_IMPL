e9da7ecdfd8584c5f741d042d38d0b03c10c8644
Naresh <nareshkadambini@gmail.com>
2021-10-17 21:21:43 -0400

    commits

--- src/training.py
+++ src/training.py
@@ -1,15 +1,17 @@
 from src.utils.common import read_config
 import argparse 
-
 from src.utils.data_mgmt import get_data
+
+
 
 def training (config_path):
     config= read_config(config_path)
-    print(config)
-    #validation_datasize = config["params"]["validation_datasize"]
+    #print(config)
 
-    #(X_train_full, y_train_full),(X_valid, y_valid),(X_test,y_test) = get_data(validation_datasize)
+    validation_datasize = config["params"]["validation_datasize"]
 
+    (X_train, y_train),(X_valid, y_valid), (X_test,y_test)= get_data(validation_datasize)
+    
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
 
